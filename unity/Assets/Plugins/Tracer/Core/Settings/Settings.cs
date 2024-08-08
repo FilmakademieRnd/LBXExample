0d1a38fa2f272d0efa526e0a5c5db4cca18c3321
Simon Spielmann <sspielma@filmakademie.de>
2024-08-08 16:58:32 +0200

    - header update

--- unity/Assets/Plugins/Tracer/Core/Settings/Settings.cs
+++ unity/Assets/Plugins/Tracer/Core/Settings/Settings.cs
@@ -1,3 +1,26 @@
+/*
+-----------------------------------------------------------------------------------
+TRACER FOUNDATION -
+Toolset for Realtime Animation, Collaboration & Extended Reality
+
+Copyright (c) 2024 Filmakademie Baden-Wuerttemberg, Animationsinstitut R&D Labs
+https://research.animationsinstitut.de/tracer 
+https://github.com/FilmakademieRnd/TRACER
+
+TRACER FOUNDATION is a development by Filmakademie Baden-Wuerttemberg,
+Animationsinstitut R&D Labs in the scope of the EU funded project
+MAX-R (101070072) and funding on the own behalf of Filmakademie Baden-Wuerttemberg.
+Former EU projects Dreamspace (610005) and SAUCE (780470) have inspired the
+TRACER FOUNDATION development.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the MIT License for more details.
+You should have received a copy of the MIT License along with this program;
+if not go to https://opensource.org/licenses/MIT
+-----------------------------------------------------------------------------------
+*/
+
 using System;
 using System.IO;
 using UnityEngine;
--- unity/Assets/Plugins/Tracer/Modules/NetworkManagerModules/UpdateSenderModule/UpdateSenderModule.cs
+++ unity/Assets/Plugins/Tracer/Modules/NetworkManagerModules/UpdateSenderModule/UpdateSenderModule.cs
@@ -1,22 +1,22 @@
 ﻿/*
-TRACER FOUNDATION - 
-Toolset for Realtime Animation, Collaboration & Extended Reality
-tracer.research.animationsinstitut.de
-https://github.com/FilmakademieRnd/TRACER
+-----------------------------------------------------------------------------------
+TRACER Location Based Experience Example
 
-Copyright (c) 2023 Filmakademie Baden-Wuerttemberg, Animationsinstitut R&D Lab
+Copyright (c) 2024 Filmakademie Baden-Wuerttemberg, Animationsinstitut R&D Labs
+https://github.com/FilmakademieRnd/LBXExample
 
-TRACER is a development by Filmakademie Baden-Wuerttemberg, Animationsinstitut
-R&D Labs in the scope of the EU funded project MAX-R (101070072) and funding on
-the own behalf of Filmakademie Baden-Wuerttemberg.  Former EU projects Dreamspace
-(610005) and SAUCE (780470) have inspired the TRACER development.
+TRACER Location Based Experience Example is a development by Filmakademie 
+Baden-Wuerttemberg, Animationsinstitut R&D Labs in the scope of the EU funded 
+project EMIL (101070533).
 
 This program is distributed in the hope that it will be useful, but WITHOUT
 ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 FOR A PARTICULAR PURPOSE. See the MIT License for more details.
-You should have received a copy of the MIT License along with this program; 
+You should have received a copy of the MIT License along with this program;
 if not go to https://opensource.org/licenses/MIT
+-----------------------------------------------------------------------------------
 */
+
 
 //! @file "UpdateSenderModule.cs"
 //! @brief Implementation of the update sender module, sending parameter updates to clients.
